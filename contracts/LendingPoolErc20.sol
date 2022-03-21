8c4e8ec0bd853217a6d20c22285d524fa60d7d1e
David Kulman <77690992+Kuly14@users.noreply.github.com>
2022-03-21 23:33:30 +0100

    FormatUpdate

--- contracts/LendingPoolErc20.sol
+++ contracts/LendingPoolErc20.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
-
     event TokensDeposited(address indexed _sender, uint indexed _amount);
     event TokensWithdrawn(address indexed _user, uint indexed _amountOfTokens);
     event ExchangeRateChanged(uint indexed _newER);
@@ -31,7 +30,6 @@ contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
     uint public utilizationRate;
     uint public constant utilizationRateKink = 80 * 10**16; // 80% utilization rate
 
-
     struct Borrower {
         uint borrowedTokens;
         uint collateral;
@@ -41,12 +39,19 @@ contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
 
     mapping(address => Borrower) internal borrowers;
 
-    constructor(string memory _name, string memory _symbol, address _token) AlphaToken(_name, _symbol) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _token
+    ) AlphaToken(_name, _symbol) {
         token = IERC20(_token);
     }
 
     function deposit(uint _amount) public nonReentrant {
-        require(token.balanceOf(msg.sender) >= _amount, "Your balance isn't sufficient");
+        require(
+            token.balanceOf(msg.sender) >= _amount,
+            "Your balance isn't sufficient"
+        );
         require(_amount > 0, "The amount can't be 0");
 
         uint toSend = _calculateErc20Tokens(_amount);
@@ -62,17 +67,17 @@ contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
         _mint(msg.sender, _amount);
     }
 
-    function _calculateErc20Tokens(uint _deposit) internal returns(uint) {
+    function _calculateErc20Tokens(uint _deposit) internal returns (uint) {
         _updateExchangeRate();
         uint toknesToSend = (_deposit * 1e18) / exchangeRate;
         return toknesToSend;
     }
 
-    function _updateExchangeRate() private { 
-        if (totalSupply() == 0) {
-            
-        } else {
-            exchangeRate = ((_availibleCash() * 1e18) + _totalBorrows() - _getReserves()) / totalSupply();
+    function _updateExchangeRate() private {
+        if (totalSupply() == 0) {} else {
+            exchangeRate =
+                ((_availibleCash() * 1e18) + _totalBorrows() - _getReserves()) /
+                totalSupply();
             emit ExchangeRateChanged(exchangeRate);
         }
     }
@@ -89,13 +94,12 @@ contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
         return totalReserves;
     }
 
-
-
     function withdraw(uint _amountInATokens) public nonReentrant {
-        require(balanceOf(msg.sender) >= _amountInATokens, "You are trying to withdraw more than you have");
+        require(
+            balanceOf(msg.sender) >= _amountInATokens,
+            "You are trying to withdraw more than you have"
+        );
         require(_amountInATokens > 0, "Amount can't be 0");
-
-
 
         uint toSend = _toSend(_amountInATokens);
 
@@ -119,51 +123,66 @@ contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
         return amountToSend;
     }
 
-    
-    function calculateUtilizationRate() internal view returns (uint) { // change to internal after testing
-        if (totalBorrows == 0) { 
+    function calculateUtilizationRate() internal view returns (uint) {
+        // change to internal after testing
+        if (totalBorrows == 0) {
             return 0;
-        } else { 
-            uint URate = totalBorrows * 1e18 / (_availibleCash() + totalBorrows - totalReserves);
+        } else {
+            uint URate = (totalBorrows * 1e18) /
+                (_availibleCash() + totalBorrows - totalReserves);
             return URate;
         }
     }
 
-    function calculateBorrowRate() internal view returns(uint) {
+    function calculateBorrowRate() internal view returns (uint) {
         if (calculateUtilizationRate() == 0) {
             return baseRate;
         } else if (calculateUtilizationRate() <= utilizationRateKink) {
-            uint bRate = baseRate + (baseSlope * calculateUtilizationRate()) / 1e18;
+            uint bRate = baseRate +
+                (baseSlope * calculateUtilizationRate()) /
+                1e18;
             return bRate;
         } else if (calculateUtilizationRate() > utilizationRateKink) {
             uint ur = calculateUtilizationRate();
             uint overUR = ur - utilizationRateKink;
-            uint rateOverKink = baseRate + (baseSlope * utilizationRateKink / 1e18) + ((overUR * slopeAfterKink) / 1e18);
+            uint rateOverKink = baseRate +
+                ((baseSlope * utilizationRateKink) / 1e18) +
+                ((overUR * slopeAfterKink) / 1e18);
             return rateOverKink;
         }
     }
 
-    function calculateSupplyRate() public view returns(uint) {
-        uint supplyRate = calculateUtilizationRate() * calculateBorrowRate() / 1e18;
+    function calculateSupplyRate() public view returns (uint) {
+        uint supplyRate = (calculateUtilizationRate() * calculateBorrowRate()) /
+            1e18;
         return supplyRate;
     }
 
-
-    function borrow(uint _amount) public nonReentrant { // _amount is the amount of tokens he wants to borrow
-        require(borrowers[msg.sender].numOfLoans == 0, "You can only take out 1 loan");
+    function borrow(uint _amount) public nonReentrant {
+        // _amount is the amount of tokens he wants to borrow
+        require(
+            borrowers[msg.sender].numOfLoans == 0,
+            "You can only take out 1 loan"
+        );
         require(_amount > 0, "The amount can't be 0");
         uint balanceOfSender = (balanceOf(msg.sender)); // Works
         uint necessaryLtv = getLtv(_amount);
-        require(balanceOfSender >= necessaryLtv, "Your balance isn't sufficient to pay the collateral");
+        require(
+            balanceOfSender >= necessaryLtv,
+            "Your balance isn't sufficient to pay the collateral"
+        );
 
         borrowers[msg.sender].borrowedTokens = _amount;
-        borrowers[msg.sender].collateral = necessaryLtv; 
+        borrowers[msg.sender].collateral = necessaryLtv;
         borrowers[msg.sender].started = block.timestamp;
         borrowers[msg.sender].numOfLoans = 1;
 
-
         require(
-            IERC20(address(this)).transferFrom(msg.sender, address(this), necessaryLtv),
+            IERC20(address(this)).transferFrom(
+                msg.sender,
+                address(this),
+                necessaryLtv
+            ),
             "Transfer didin't go through"
         );
 
@@ -176,26 +195,24 @@ contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
         emit TokensBorrowed(_amount, msg.sender);
     }
 
-
-
-    function getLtv(uint _amount) public view returns(uint) {
-        uint coll = _amount * LtvRate / 10000;
+    function getLtv(uint _amount) public view returns (uint) {
+        uint coll = (_amount * LtvRate) / 10000;
         uint amountToReceive = (coll * 1e18) / exchangeRate;
         return amountToReceive;
-
     }
 
     function repay() public nonReentrant {
-        require(borrowers[msg.sender].numOfLoans == 1, "You don't have any tokens borrowed");
+        require(
+            borrowers[msg.sender].numOfLoans == 1,
+            "You don't have any tokens borrowed"
+        );
         uint amountToPayBack = _calculateInterestOfBorrower();
         uint collateral = borrowers[msg.sender].collateral;
 
-
         borrowers[msg.sender].borrowedTokens = 0;
-        borrowers[msg.sender].collateral = 0; 
+        borrowers[msg.sender].collateral = 0;
         borrowers[msg.sender].started = 0;
         borrowers[msg.sender].numOfLoans = 0;
-
 
         require(
             token.transferFrom(msg.sender, address(this), amountToPayBack),
@@ -211,13 +228,16 @@ contract LendingPoolErc20 is AlphaToken, ReentrancyGuard {
         emit TokensRepayed(amountToPayBack, msg.sender);
     }
 
-    function _calculateInterestOfBorrower() internal view returns (uint) { // change to internal after testing
+    function _calculateInterestOfBorrower() internal view returns (uint) {
+        // change to internal after testing
         uint borrowRate = calculateBorrowRate();
         uint interestPerBlock = borrowRate / blocksPerYear;
         uint numOfBlocks = block.timestamp - borrowers[msg.sender].started;
-        uint interestForTheCaller = numOfBlocks * interestPerBlock; 
-        uint interestInTokens = (borrowers[msg.sender].borrowedTokens * interestForTheCaller) / 1e18;
-        uint amountToPayBack = interestInTokens + borrowers[msg.sender].borrowedTokens;
+        uint interestForTheCaller = numOfBlocks * interestPerBlock;
+        uint interestInTokens = (borrowers[msg.sender].borrowedTokens *
+            interestForTheCaller) / 1e18;
+        uint amountToPayBack = interestInTokens +
+            borrowers[msg.sender].borrowedTokens;
         return amountToPayBack;
     }
 }
